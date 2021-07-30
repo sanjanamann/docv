8a179b534a9a969c46e2c1b7a772b56ac27414ab
Sanjanamann <sanjanamann1995@gmail.com>
2021-07-30 21:30:48 +0530

    docv

---
+++ app/src/main/java/com/example/docv/putpdf.java
@@ -0,0 +1,32 @@
+package com.example.docv;
+
+public class putpdf {
+    public String name;
+    public String url;
+
+    public putpdf()
+    {
+
+    }
+    public putpdf(String name, String url)
+    {
+        this.name=name;
+        this.url=url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ app/src/main/java/com/example/docv/successful.java
@@ -0,0 +1,34 @@
+package com.example.docv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+
+
+public class successful extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;      // Delay of 3 Seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(successful.this, verificationpage.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
+
